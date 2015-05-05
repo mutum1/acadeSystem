3539b37390721e0435ad9661a51f31d10978c58b
paulo <paulo@172.20.111.234>
2015-05-05 15:59:10 -0300

    varias alteraçoes

---
+++ acadeSystem/src/br/ufes/ceunes/poo/model/dao/DisciplinaDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufes.ceunes.poo.model.dao;
+
+import br.ufes.ceunes.poo.model.pojo.Disciplina;
+import br.ufes.ceunes.poo.model.pojo.Professor;
+import java.util.List;
+
+/**
+ *
+ * @author paulo
+ */
+public interface DisciplinaDao {
+    public void adicionar(Disciplina disciplina);
+    public List getAll();
+    public void carregar();
+    public void salvar();
+    
+}
