aebc4cbf9104fe563afd0589963aa6bba61ebb81
Rhaister <Rhaister@192.168.1.8>
2015-06-28 23:35:15 -0300

    faltas

--- acadeSystem/src/br/ufes/ceunes/poo/model/dao/FaltasDaoImpl.java
+++ acadeSystem/src/br/ufes/ceunes/poo/model/dao/FaltasDaoImpl.java
@@ -5,11 +5,20 @@
  */
 package br.ufes.ceunes.poo.model.dao;
 
+import br.ufes.ceunes.poo.model.pojo.Aluno;
+import br.ufes.ceunes.poo.model.pojo.Atividade;
 import br.ufes.ceunes.poo.model.pojo.Faltas;
+import br.ufes.ceunes.poo.model.pojo.Turma;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,6 +26,8 @@ import java.util.List;
  */
 public class FaltasDaoImpl implements FaltasDao{
     private List<Faltas> listaFaltas;
+    private TurmaDao turmaAcoes;
+    private AlunoDao alunoAcoes;
     int id;
     /**
      * Carrega do arquivo todas as faltas e coloca em uma lista 
@@ -35,80 +46,69 @@ public class FaltasDaoImpl implements FaltasDao{
             String idTemp = ler.readLine();
             id = Integer.parseInt(idTemp)+1;
             while(ler.ready()){//Equando nao chegar no final do arquivo, while continua
-                String aluno = ler.readLine();//Pega nome
-                String turma = ler.readLine();//Pega turma
-               // String cargaHoraria = ler.readLine(); //cargaHoraria
-                String id = ler.readLine(); 
-                Integer nIds;
-                String nIdTemp = ler.readLine();
-                nIds = Integer.parseInt(nIdTemp);
-                Faltas falta = new Faltas(Integer.parseInt(id), aluno, turma);
-                int i;
-                for(i=0;i<nIds;i++){
-                    nIdTemp = ler.readLine();
-                    int idsTemp= Integer.parseInt(nIdTemp);
-                    Professor professor=new Professor(null,null,null,idsTemp);
-                    professor = professorDao.buscar(professor);
-                    novaDisciplina.addProfessor(professor);
-                    professor.addDiscilina(novaDisciplina);
-                }
+                idTemp = ler.readLine(); 
+                int idInt = Integer.parseInt(idTemp);
+                            
+                String idBusca = ler.readLine(); //pega id da turma
+                int idBusc = Integer.parseInt(idBusca);
                 
-                adicionar(novaDisciplina);//Adiciona na lista
-            }
+                Turma turma = turmaAcoes.buscar(new Turma(null,null,null,null,0,null,null,idBusc));
+                
+                idBusca =ler.readLine();
+                idBusc = Integer.parseInt(idBusca);
+                
+                Aluno aluno = alunoAcoes.buscar(new Aluno(null,null,idBusc));
+                
+                Faltas novaFalta = new Faltas(idInt, aluno,turma);      
+                
+                listaFaltas.add(novaFalta);
+            }    
             file.close();//Fecho o arquivo
             ler.close();           
         } catch (FileNotFoundException ex) {//Coisa do NetBeans
             id=1;          
-            
         } catch (IOException ex) {//Coisa do NetBeans
-            Logger.getLogger(ProfessorDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(AlunoDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
         } catch (NumberFormatException ex){
             id=1;
             return;
-        }     
+        } 
     }
     /**
      * Salva todas as disciplinas de uma lista em um arquivo Disciplina.txt.
-     * @param disciplinaAdd lista de disciplinas.
+     * @param faltas
      * Ativda exceções FileNotFoundException, IOException.
      */
     @Override
-    public void salvar(Faltas falta){
-        adicionar(disciplinaAdd);
+    public void salvar(Faltas faltas) {
         
-        String nomeArquivo = "Disciplinas.txt";//Nome do arquivo
+        listaFaltas.add(faltas);
+        
+        String nomeArquivo = "Faltas.txt";//Nome do arquivo
         try {
             FileWriter file = new FileWriter(nomeArquivo,false);//Abro o arquivo para salvar
             BufferedWriter salvar = new BufferedWriter(file);//Estacio o arquivo para salvar
+            
             salvar.write(Integer.toString(id));
             salvar.newLine();
-            for(Disciplina disciplina : listaDisciplina){//percorre a lista
-                salvar.write(disciplina.getNome());//salva nome
-                salvar.newLine();
-                salvar.write(disciplina.getEmenta());//salva a ementa
-                salvar.newLine();
-                salvar.write(disciplina.getCargaHoraria());//salva carga horaria
-                salvar.newLine();
-                salvar.write(Integer.toString(disciplina.getId()));//salva o id da disciplina
-                salvar.newLine();
-                String numerosDeCpdfs=""+disciplina.listaProfessor().size();
+            
+            for(Faltas falta : listaFaltas){//percorre a lista
                 
-                salvar.write(numerosDeCpdfs);//quantos cpfs tem na lista de cpfs
-                
+                salvar.write(Integer.toString(falta.getId()));//id
                 salvar.newLine();
-                for(Professor professor : disciplina.listaProfessor() ){ 
-                    salvar.write(professor.getId());//salva um por um o q esta na lista de professores
-                    salvar.newLine();
-                }
-                
+                salvar.write(Integer.toString(falta.getTurma().getId()));//salva id da turma
+                salvar.newLine();
+                salvar.write(Integer.toString(falta.getAluno().getId()));//salva id do aluno
+                salvar.newLine();
             }
             salvar.close();
             file.close();            
         } catch (FileNotFoundException ex) {//Coisa do NetBeans
-            Logger.getLogger(DisciplinaDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(AtividadeDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {//Coisa do NetBeans
-            Logger.getLogger(DisciplinaDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }  
+            Logger.getLogger(AtividadeDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
         carregar();
-    }    
+    }
 }
+    
